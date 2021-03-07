586ccd45c51ad85f68655ba77090f00787407c8f
Abhinav Kumar <abhinavkrabhi921@gmail.com>
2021-03-08 01:50:13 +0530

    dynamic done and updated makefile

--- Makefile
+++ Makefile
@@ -1,20 +1,24 @@
 C=g++
 CFLAGS= -g -Wall
 
-TARGET=task1
+TARGET1=task1
+TARGET2=task2
 
 RM=rm -rf
 
 LIB=`pkg-config --cflags --libs opencv`
 
-all: $(TARGET)
+all: $(TARGET1) $(TARGET2)
 
-$(TARGET): $(TARGET).cpp 
-	@$(C) $(CFLAGS) -o $(TARGET) $(TARGET).cpp $(LIB)
+$(TARGET1): $(TARGET1).cpp 
+	@$(C) $(CFLAGS) -o $(TARGET1) $(TARGET1).cpp $(LIB)
 
+$(TARGET2): $(TARGET2).cpp $(TARGET1).h
+	@$(C) $(CFLAGS) -o $(TARGET2) $(TARGET2).cpp $(TARGET1).h $(LIB)
+	
 
 clean:
-	@$(RM) $(TARGET)
+	@$(RM) $(TARGET1) $(TARGET2)
 
 .DEFAULT:
 	@echo "Wrong Command"
@@ -25,6 +29,6 @@ help:
 	@echo "help menu:"
 	@echo "make help               Show this help"
 	@echo "make clean              Remove all intermediate files"
-	@echo "make all                Compile the program"
+	@echo "make all                Compile the frogram"
 	@echo "./task1 image_name      Run the program but first you have to compile it"
 
--- task2.cpp
+++ task2.cpp
@@ -1,36 +1,43 @@
+#include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
 #include "task1.h"
-#include <iostream>
-#include <sstream>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/video.hpp>
+
 using namespace cv;
 using namespace std;
-int main(int argc, char* argv[])
-{   
-    Ptr<BackgroundSubtractor> pBackSub;
+
+void queuedensity_dynamicdensity(String file){
+	ofstream MyFile("out.txt");
+	Ptr<BackgroundSubtractor> pBackSub;
     pBackSub = createBackgroundSubtractorKNN();
-    VideoCapture capture(argv[1]);
+    VideoCapture capture(file);
     if (!capture.isOpened()){
         //error in opening the video input
         cerr << "Unable to open video "<< endl;
-        return 0;
+        return;
     }
     Mat frame, fgMask;
     imgHomography H1 = fun("empty1.png");
     for(int i=0; i<20; i++){
-        pBackSub->apply(imread("empty1Cropped.png"), fgMask, 0.5);	
+        pBackSub->apply(imread("empty1.png"), fgMask, 0.5);	
     }
-    cout<<"Frame Number"<<"\t"<<"Queue Density (MAX 100)"<<"\t"<<"Dynamic Density (MAX 100)"<<"\n";
+    
+    Mat frame0, frame0_gray;
+    capture >> frame0;
+	Mat frame0_crop =fun2(frame0,H1);
+	cvtColor(frame0_crop, frame0_gray, COLOR_BGR2GRAY);
+    Mat fr;
+    capture>> fr;
+    
+    cout<<"framenum"<<"\t"<<"queue density"<<"\t"<<"dynamic density"<<"\n";
+    MyFile <<"framenum"<<","<<"queue density"<<","<<"dynamic density"<<"\n";
     while (true) {
         capture >> frame;
         if (frame.empty())
             break;
         //update the background model
         frame=fun2(frame,H1);
-        pBackSub->apply(frame, fgMask, 0.00001);
+        pBackSub->apply(frame, fgMask,0.00001);
+        //pBackSub->apply(frame, fgMask);
         //get the frame number and write it on the current frame
         rectangle(frame, cv::Point(10, 2), cv::Point(100,20),
                   cv::Scalar(255,255,255), -1);
@@ -40,16 +47,62 @@ int main(int argc, char* argv[])
         putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
                 FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
         //show the current frame and the fg masks
-        threshold(fgMask,fgMask,254,255,THRESH_BINARY);
-        cout<<frameNumberString<<"\t\t"<<double(countNonZero(fgMask))*100.00/double(fgMask.rows)/double(fgMask.cols)<<"\n";
-        imshow("Frame", frame);
+        threshold(fgMask,fgMask,127,255,THRESH_BINARY);
+        //imshow("frame", frame);
         imshow("FG Mask", fgMask);
+        
+        
+        //dynamic part
+        Mat frame_gray;
+        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+        imshow("frame", frame_gray);
+		//Mat frame_crop = fun3(frame_gray,H);
+		
+		Mat flow(frame0_gray.size(), CV_32FC2);
+        calcOpticalFlowFarneback(frame0_gray, frame_gray, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+        // visualization
+        Mat flow_parts[2];
+        split(flow, flow_parts);
+        Mat magnitude, angle, magn_norm;
+        cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
+        normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
+        angle *= ((1.f / 360.f) * (180.f / 255.f));
+        threshold(magnitude,magnitude,10,255,THRESH_BINARY);
+
+        //build hsv image
+        Mat _hsv[3], hsv, hsv8, bgr;
+        _hsv[0] = angle;
+        _hsv[1] = Mat::ones(angle.size(), CV_32F);
+        _hsv[2] = magn_norm;
+        merge(_hsv, 3, hsv);
+        hsv.convertTo(hsv8, CV_8U, 255.0);
+        cvtColor(hsv8, bgr, COLOR_HSV2BGR);
+        //imshow("ff",magn_norm);
+        imshow("frame_gray", magnitude);
+        //imshow("frame_hsv",bgr);
+        frame0_gray=frame_gray;
+        
+        //print
+        cout<<frameNumberString<<"\t\t"<<double(countNonZero(fgMask))/double(fgMask.rows)/double(fgMask.cols)<<"\t\t"<<double(countNonZero(magnitude))/double(magnitude.rows)/double(magnitude.cols)<<"\n";
+        MyFile <<frameNumberString<<","<<double(countNonZero(fgMask))/double(fgMask.rows)/double(fgMask.cols)<<","<<double(countNonZero(magnitude))/double(magnitude.rows)/double(magnitude.cols)<<"\n";
         //get the input from the keyboard
         int keyboard = waitKey(30);
         if (keyboard == 'q' || keyboard == 27)
             break;
-    capture >> frame;
-    capture >> frame;
+    	capture >> frame;
+    	capture >> frame;
     }
+    MyFile.close();
+    return;
+}
+
+
+int main(int argc, char* argv[]){
+    queuedensity_dynamicdensity(argv[1]);
     return 0;
 }
+
+
+
+
+
